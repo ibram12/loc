13f15fb8e5c388e13cd02690a11f404604b288c4
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-04-22 17:46:01 +0200

    create simple Bloc observer file to track cubits changes

--- lib/featuers/auth/presentation/views/sginup_view.dart
+++ lib/featuers/auth/presentation/views/sginup_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../manager/cubits/signUp_cubit/sign_up_cubit.dart';
 import '../widgets/signup_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -9,8 +10,11 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SginUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignUpCubit(),
+      child: const Scaffold(
+        body: SginUpViewBody(),
+      ),
     );
   }
 }
