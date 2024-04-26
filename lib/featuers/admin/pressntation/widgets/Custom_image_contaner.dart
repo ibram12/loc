10fe13d5e51c7737f8bb40e3fd9b323983fa2e82
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-04-26 18:02:23 +0200

    Add image picker functionality and file selector plugins
    - Added image picker functionality to allow users to select images from camera or gallery

--- lib/featuers/admin/pressntation/widgets/Custom_image_contaner.dart
+++ lib/featuers/admin/pressntation/widgets/Custom_image_contaner.dart
@@ -1,10 +1,31 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:loc/core/text_styles/Styles.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:loc/featuers/admin/pressntation/widgets/custom_bottom_sheet_body.dart';
 
-class CustomImageContaner extends StatelessWidget {
+class CustomImageContaner extends StatefulWidget {
   const CustomImageContaner({super.key});
+
+  @override
+  State<CustomImageContaner> createState() => _CustomImageContanerState();
+}
+
+class _CustomImageContanerState extends State<CustomImageContaner> {
+  File? image;
+
+  Future<void> pickImagef(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        image = File(pickedFile.path);
+      });
+      Navigator.pop(context);
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +35,14 @@ class CustomImageContaner extends StatelessWidget {
         showModalBottomSheet(
             context: context,
             builder: (context) {
-              return const BottomSheetBody();
+              return BottomSheetBody(
+                chooseCamera: () {
+                  pickImagef(ImageSource.camera);
+                },
+                chooseGallery: () {
+                  pickImagef(ImageSource.gallery);
+                },
+              );
             });
       },
       child: Container(
@@ -24,10 +52,15 @@ class CustomImageContaner extends StatelessWidget {
         decoration: BoxDecoration(
             border: Border.all(color: Colors.black, width: 2),
             borderRadius: BorderRadius.circular(10)),
-        child: const Icon(
-          Icons.camera_alt_outlined,
-          size: 50,
-        ),
+        child: image == null
+            ? const Icon(
+                Icons.camera_alt_outlined,
+                size: 50,
+              )
+            : Image.file(
+                image!,
+                fit: BoxFit.cover,
+              ),
       ),
     );
   }
--- lib/featuers/admin/pressntation/widgets/custom_bottom_sheet_body.dart
+++ lib/featuers/admin/pressntation/widgets/custom_bottom_sheet_body.dart
@@ -4,43 +4,48 @@ import 'package:flutter/material.dart';
 import '../../../../core/text_styles/Styles.dart';
 
 class BottomSheetBody extends StatelessWidget {
-  const BottomSheetBody({super.key});
+  const BottomSheetBody(
+      {super.key, required this.chooseGallery, required this.chooseCamera});
+  final void Function() chooseGallery;
+  final void Function() chooseCamera;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.only(top: 25,bottom: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CircleAvatar(
-                          radius: 25,
-                          backgroundColor: Colors.grey.shade300,
-                          child: const Icon(Icons.camera_alt),
-                        ),
-                      const Text(
-                        'Camera',
-                        style: Styles.textStyle14,
-                      )
-                    ]),
-                      Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CircleAvatar(
-                          radius: 25,
-                          backgroundColor: Colors.grey.shade300,
-                          child: const Icon(Icons.image),
-                        ),
-                      const Text(
-                        'Gallery',
-                        style: Styles.textStyle14,
-                      )
-                    ]),
-                  ],
-                ),
-              );
+      padding: const EdgeInsets.only(top: 25, bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          InkWell(
+            onTap: chooseCamera,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.grey.shade300,
+                child: const Icon(Icons.camera_alt),
+              ),
+              const Text(
+                'Camera',
+                style: Styles.textStyle14,
+              )
+            ]),
+          ),
+          InkWell(
+            onTap: chooseGallery,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.grey.shade300,
+                child: const Icon(Icons.image),
+              ),
+              const Text(
+                'Gallery',
+                style: Styles.textStyle14,
+              )
+            ]),
+          ),
+        ],
+      ),
+    );
   }
 }
