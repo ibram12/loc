fe2fe77efcd5ce514bf894c7b980e8d7e0ab4f7c
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-05-08 13:11:34 +0200

    Update and reply state admin alert dialog to display dynamic content - admin now be able to change the replay state for each his list of reservatoins and for user who sent this request - Update dialog content to display dynamic information based on request model

--- lib/core/helper/admin_alert_dialog.dart
+++ lib/core/helper/admin_alert_dialog.dart
@@ -1,14 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+
+import '../../featuers/admin/data/models/request_model.dart';
 
 void adminAlrtDialog(
-    {required BuildContext context, required Function() onAccept, required Function() onReject}) {
+    {required BuildContext context, required Function() onAccept, required Function() onReject,required RequestModel requestModel,required String hallName}) {
 showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Alert'),
-          content: const Text('ramy needs to book hall 1 from 10:00 to 12:00'),
+          content: RichText(
+  text: TextSpan(
+    children: [
+      TextSpan(
+        text: requestModel.name,
+          style: const TextStyle(
+          fontWeight: FontWeight.bold, 
+          fontSize: 16,
+          color: Colors.black, 
+        ),
+      ),
+      TextSpan(
+        text: ' needs to book $hallName on ${requestModel.sendDate} from ',
+        style: const TextStyle(
+          fontWeight: FontWeight.normal,
+          fontSize: 16, 
+          color: Colors.black, 
+        ),
+      ),
+      TextSpan(
+        text: DateFormat.jm().format(requestModel.startTime.toDate()),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold, 
+          fontSize: 16,
+          color: Colors.black, 
+        ),
+      ),
+      const TextSpan(
+        text: ' to ',
+        style: TextStyle(
+          fontWeight: FontWeight.normal,
+          fontSize: 16, 
+          color: Colors.black, 
+        ),
+      ),
+      TextSpan(
+        text: DateFormat.jm().format(requestModel.endTime.toDate()),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold, 
+          fontSize: 16, 
+          color: Colors.black, 
+        
+        ),
+      ),
+    ],
+  ),
+),
           actions: [
             TextButton(
               onPressed: onAccept,
