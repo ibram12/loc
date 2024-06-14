d89b47e4db15dea657cb602b4b9bdc79ed4769bc
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-06-14 22:39:39 +0200

    refactor all requests listView

--- lib/featuers/admin/pressntation/widgets/all_requests_view_body.dart
+++ lib/featuers/admin/pressntation/widgets/all_requests_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loc/core/text_styles/Styles.dart';
 import 'package:loc/featuers/admin/pressntation/widgets/custom_divider.dart';
 import 'package:loc/featuers/admin/pressntation/widgets/request_item.dart';
 
@@ -31,45 +32,56 @@ class _AllRequestsViewBodyState extends State<AllRequestsViewBody> {
           setState(() {
             hallNameToId = {
               for (var i = 0; i < state.allHallsNames.length; i++)
-              state.allHallRequests[i]  : state.allHallsNames[i]
+                state.allHallRequests[i]: state.allHallsNames[i]
             };
           });
         }
       },
       builder: (context, state) {
+        if (state is GetAllHallReservaionsFailer) {
+          return Center(
+            child: Text(
+              state.massege,
+              style: Styles.textStyle18,
+            ),
+          );
+        }
         return CustomScrollView(
           slivers: hallNameToId.entries.map((entry) {
-            return SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  CustomDiveder(hallName: entry.key),
-                  StreamBuilder<QuerySnapshot<Object?>>(
-                    stream: FirebaseFirestore.instance
-                        .collection("locs")
-                        .doc(entry.value)
-                        .collection("reservations")
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      return ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          final requestModel = RequestModel.fromDocumentSnapshot(snapshot.data!.docs[index]);
-                          return RequestItem(
-                            requestModel: requestModel,
-                            reservationId: snapshot.data!.docs[index].id,
-                            hallId: entry.value,
-                            hallName: entry.key,
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CustomDiveder(hallName: entry.key),
+                      StreamBuilder<QuerySnapshot<Object?>>(
+                        stream: FirebaseFirestore.instance
+                            .collection("locs")
+                            .doc(entry.value)
+                            .collection("reservations")
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return const Center(child: CircularProgressIndicator());
+                          }
+                          return Column(
+                            children: snapshot.data!.docs.map((doc) {
+                              final requestModel = RequestModel.fromDocumentSnapshot(doc);
+                              return RequestItem(
+                                requestModel: requestModel,
+                                reservationId: doc.id,
+                                hallId: entry.value,
+                                hallName: entry.key,
+                              );
+                            }).toList(),
                           );
                         },
-                      );
-                    },
-                  ),
-                ],
+                      ),
+                    ],
+                  );
+                },
+                childCount: 1,
               ),
             );
           }).toList(),
