e51ddc73b1bd661e62bc7d435e0575e3e2656d56
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-06-20 22:46:44 +0200

    refactor

--- lib/featuers/admin/pressntation/view/reservation_table_view.dart
+++ lib/featuers/admin/pressntation/view/reservation_table_view.dart
@@ -27,52 +27,54 @@ class _ReservatoinTableViewState extends State<ReservatoinTableView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-          stream: stream,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.data == null) {
-              return  Center(child: Text(
-              S.of(context).no_data_found
-              ));
-            }
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  PaginatedDataTable(
-                    columns:  [
-                      DataColumn(label: Text(S.of(context).name)),
-                      DataColumn(label: Text(S.of(context).service)),
-                      DataColumn(label: Text(S.of(context).date)),
-                      DataColumn(label: Text(S.of(context).start_time)),
-                      DataColumn(label: Text(S.of(context).end_time)),
-                      DataColumn(label: Text(S.of(context).delete)),
-                    ],
-                    rowsPerPage: 10,
-                    header: Row(
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            icon: const Icon(Icons.arrow_back)),
-                         Text(
-                          S.of(context).reservations,
-                          style: Styles.textStyle20,
-                        ),
+    return SafeArea(
+      child: Scaffold(
+        body: StreamBuilder(
+            stream: stream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.data == null) {
+                return  Center(child: Text(
+                S.of(context).no_data_found
+                ));
+              }
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    PaginatedDataTable(
+                      columns:  [
+                        DataColumn(label: Text(S.of(context).name)),
+                        DataColumn(label: Text(S.of(context).service)),
+                        DataColumn(label: Text(S.of(context).date)),
+                        DataColumn(label: Text(S.of(context).start_time)),
+                        DataColumn(label: Text(S.of(context).end_time)),
+                        DataColumn(label: Text(S.of(context).delete)),
                       ],
-                    ),
-                    source: MyData(
-                       context: context,
-                      data: snapshot.data!.docs,
-                    ),
-                  )
-                ],
-              ),
-            );
-          }),
+                      rowsPerPage: 10,
+                      header: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              icon: const Icon(Icons.arrow_back)),
+                           Text(
+                            S.of(context).reservations,
+                            style: Styles.textStyle20,
+                          ),
+                        ],
+                      ),
+                      source: MyData(
+                         context: context,
+                        data: snapshot.data!.docs,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }),
+      ),
     );
   }
 }
